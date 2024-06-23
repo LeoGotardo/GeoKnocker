e1702db6e8b7351c0abe42e9d22b3f3dc3751a69
LeoGotardo <leonardo.gotardo2@gmail.com>
2024-06-23 18:24:42 -0300

    add geo location

--- portScanner.py
+++ portScanner.py
@@ -8,30 +8,31 @@ import sys
 import os
 
 class PortScan:
-    @calc
     @staticmethod
-    def scanPorts(ip, port_option, *rangePorts):
+    def scanPorts(**kwargs):
         os.system('cls' if os.name == 'nt' else 'clear')
+        ic(kwargs)
+
         try:
             validParameters = ['-a', '-m']
             
-            if port_option in validParameters:
-                if port_option.lower() == "-a":
-                    if rangePorts:
-                        ports = list(range(rangePorts[0], rangePorts[1] + 1))
+            if kwargs["port_option"] in validParameters:
+                if kwargs["port_option"].lower() == "-a":
+                    if "rangePorts" in kwargs:
+                        ports = list(range(kwargs['rangePorts'][0], kwargs["rangePorts"][1] + 1))
                     else:
                         ports = list(range(1, 65535))
                     
-                if port_option.lower() == "-m":
+                if kwargs["port_option"].lower() == "-m":
                     ports = [20, 21, 22, 23, 25, 53, 80, 443, 1194, 3306, 5000, 5432, 8000, 8080, 8291, 8728, 9050]
                     
                 openPorts = []
-                with alive_bar(len(ports), title="Kniking doors...") as bar:
+                with alive_bar(len(ports), title="Knocking doors...") as bar:
                     for port in ports:
                         try:
                             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                                 s.settimeout(0.8)
-                                result = s.connect_ex((ip, port))
+                                result = s.connect_ex((kwargs["ip"], port))
                                 if result == 0:
                                     print(f"[+] {port} --> OPEN")
                                     openPorts.append([port, 'open'])
@@ -44,15 +45,19 @@ class PortScan:
                             print('[!] Sniffer interrupted')
                             quit()
                         bar()
-                return openPorts
+                if kwargs['geo'] is not None:
+                    locate = geoLocate.getGeo(kwargs['ip'])
+                    return openPorts, locate
+                else:
+                    return openPorts, None
             else:
-                error = '[!] The options are -a for all ports and -m for main ports!'
+                error = '[!] The options are -a for all ports, -m for main ports and -g for geolocation!'
                 print(error)
-                return error
+                return error, None
 
         except Exception as e:
             print(f"Error: {e}")
-            return str(e)
+            return str(e), None
 
 class geoLocate:
     @staticmethod
@@ -64,7 +69,8 @@ class geoLocate:
             if 'error' not in data:
                 info = {'country': data['country'],
                         'city':data['city'],
-                        'location':data['loc'],
+                        'lat':data['loc'].split(',')[0],
+                        'lon':data['loc'].split(',')[1],
                         'timezone':data['timezone'],
                         'org':data['org'],
                         'hostname':data['hostname']}
